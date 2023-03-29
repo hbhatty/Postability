57d355e629d4148415b9aeb96aea66809f59289d
Harsh Bhatty <harshbhatty2@gmail.com>
2023-03-29 00:33:18 -0400

    fixed errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+//   images: {
+//     domains: ["lh3.googleusercontent.com"]
+//   }
+// }
+
+module.exports = {
+   webpack: config => {
+      config.resolve.preferRelative = true
+      return config
+   },
+   experimental: {
     appDir: true,
   },
   images: {
     domains: ["lh3.googleusercontent.com"]
   }
 }
-
-module.exports = nextConfig
